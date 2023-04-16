5bb7901d53aadf36929e01bccb1acedcf2206e6a
David <dabojimenez16@hotmail.com>
2023-04-15 20:46:45 -0500

    Se agrega una nueva carpeta llamada helpers, en la cual se agrega la funcion de llamado a la API de los gifs, la cual se exporta para ser usada en el archvio gifGrid. La funcion recive un argumento, que es el valor del input

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,11 @@
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ( {category}) => {
+
+    
+
+    getGifs(category);
+
   return (
     <>
         <h3>{category}</h3>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,11 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=OxjjAn7Si1An7ZtkF2xdyoRDujEz7cr5&q=${category}&limit=10`;
+    const respons = await fetch( url)
+    const {data} = await respons.json();
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    console.log(gifs);
+}
