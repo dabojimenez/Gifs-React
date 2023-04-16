46dc3e656fae64835010b9340c4cc5be5eb83372
David <dabojimenez16@hotmail.com>
2023-04-15 21:46:55 -0500

    Se hace uso de useEffect, para que no vuelva a realizar una consulta http al renderizar por primera vez nuestro componente

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
-export const GifGrid = ( {category}) => {
+export const GifGrid = ({ category }) => {
 
-    
+    const [counter, setCounter] = useState(10)
 
-    getGifs(category);
+    //cuando se renderice por primera vez, solo ahi realizaremos la peticion http
+    //recibe dos argumentos, 
+    //1 el efecto que queremos disparar,qe es un callback
+    //2 liosta de dependencias, que son las condiciones en las cuales  queremos ejecutar el calback, el cual es opcional pero se recomienda enviar
+    useEffect( () => {
+        getGifs(category);
+        //si yo dejo la lista de dependencias vacias, quiere decir q solo se va a disparar la primera vez que se crea nuestro componente
+    }, [] )
+    // getGifs(category);
 
-  return (
-    <>
-        <h3>{category}</h3>
-        <p></p>
-    </>
-  )
+    return (
+        <>
+            <h3>{category}</h3>
+            <h5>{counter}</h5>
+            <button onClick={() => setCounter(counter + 1)}>+1</button>
+        </>
+    )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,4 +1,5 @@
 export const getGifs = async(category) => {
+
     const url = `https://api.giphy.com/v1/gifs/search?api_key=OxjjAn7Si1An7ZtkF2xdyoRDujEz7cr5&q=${category}&limit=10`;
     const respons = await fetch( url)
     const {data} = await respons.json();
@@ -8,4 +9,6 @@ export const getGifs = async(category) => {
         url: img.images.downsized_medium.url
     }))
     console.log(gifs);
+    
+    return gifs;
 }
